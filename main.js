0b35b90bcd5fba2d0a7f3f3d4ea5d23487c627c3
Sidnei M Silva Filho <sialkas@gmail.com>
2019-07-30 12:57:23 -0300

    Exemplo de classe

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-alert('Teste');
+// Class TodoList
+class TodoList{
+ constructor(){
+   this.todos = [];
+ } 
+
+ // Método addTodo
+ addTodo(){
+   this.todos.push('item');
+   console.log(this.todos);
+ }
+
+}
+
+const MinhaLista = new TodoList();
+
+document.getElementById('adicionar').onclick = function() {
+  MinhaLista.addTodo();
+}
